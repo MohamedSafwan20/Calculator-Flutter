08459b514ed0ac6f8a877df6d11185c8a68cee23
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2022-03-24 16:07:35 +0530

    Added UI for home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:calculator/config/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -32,10 +31,36 @@ class _HomePageState extends State<HomePage> {
             child: Container(
               width: double.infinity,
               color: isDarkTheme ? Colors.black : Colors.white,
+              padding: const EdgeInsets.only(right: 30, bottom: 20),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.end,
-                children: [Text("892 x 100"), Text("69655")],
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: RichText(
+                      text: TextSpan(
+                        text: '892  ',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w800,
+                            color: isDarkTheme ? Colors.white : Colors.black),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: 'x',
+                              style: TextStyle(
+                                  color:
+                                      Theme.of(context).colorScheme.primary)),
+                          const TextSpan(text: '  100'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Text(
+                    "69655",
+                    style: TextStyle(fontSize: 45, fontWeight: FontWeight.w900),
+                  )
+                ],
               ),
             ),
           ),
@@ -44,10 +69,310 @@ class _HomePageState extends State<HomePage> {
             fit: FlexFit.tight,
             child: Container(
               width: double.infinity,
-              color: isDarkTheme
-                  ? darkColors["disabled"]
-                  : lightColors["disabled"],
-              child: Text("df"),
+              color: Theme.of(context).colorScheme.background,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "C",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Icon(Icons.backspace_outlined,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondary),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Icon(Icons.percent,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondary),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: const Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "/",
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ))),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "7",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: isDarkTheme
+                                            ? Colors.white
+                                            : Colors.black),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "8",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: isDarkTheme
+                                            ? Colors.white
+                                            : Colors.black),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "9",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: isDarkTheme
+                                            ? Colors.white
+                                            : Colors.black),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: const Align(
+                                  alignment: Alignment.center,
+                                  child: Icon(
+                                    Icons.close_outlined,
+                                    size: 30,
+                                  ),
+                                ))),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "4",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: isDarkTheme
+                                            ? Colors.white
+                                            : Colors.black),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "5",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: isDarkTheme
+                                            ? Colors.white
+                                            : Colors.black),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "6",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: isDarkTheme
+                                            ? Colors.white
+                                            : Colors.black),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: const Align(
+                                  alignment: Alignment.center,
+                                  child: Icon(
+                                    Icons.remove_outlined,
+                                    size: 30,
+                                  ),
+                                ))),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "1",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: isDarkTheme
+                                            ? Colors.white
+                                            : Colors.black),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "2",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: isDarkTheme
+                                            ? Colors.white
+                                            : Colors.black),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "3",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: isDarkTheme
+                                            ? Colors.white
+                                            : Colors.black),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: const Align(
+                                  alignment: Alignment.center,
+                                  child: Icon(
+                                    Icons.add_outlined,
+                                    size: 30,
+                                  ),
+                                ))),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: TextButton(
+                                onPressed: null,
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: isDarkTheme
+                                            ? Colors.white
+                                            : Colors.black),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "0",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: isDarkTheme
+                                            ? Colors.white
+                                            : Colors.black),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    ".",
+                                    style: TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700,
+                                        color: isDarkTheme
+                                            ? Colors.white
+                                            : Colors.black),
+                                  ),
+                                ))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: const Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "=",
+                                    style: TextStyle(
+                                        fontSize: 42,
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                ))),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
